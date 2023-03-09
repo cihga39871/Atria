6473325b9b7b8c30eeb2242f32016a9b5c9eb35a
Jiacheng Chuan <jiacheng_chuan@outlook.com>
2023-03-09 14:18:28 -0500

    fit test bit match

--- src/AtriaTest/BioBits/bit_match.jl
+++ src/AtriaTest/BioBits/bit_match.jl
@@ -13,14 +13,14 @@
         j = dna"GGTCAGTACGTCAGTACGCAGTAGTGTATTTACC" |> bitsafe!
     
         # to speed up, bitwise_scan does not handle tail match well.
-        @test bitwise_scan(a, b, 1, 0) == (4, 16)
-        @test bitwise_scan(a, c, 1, 0) == (5, 16)
-        @test bitwise_scan(a, d, 1, 0) == (33, 4)
-        @test bitwise_scan(a, e, 1, 0) == (31, 4) # actually best is 32,4
-        @test bitwise_scan(a, f, 1, 5) == (32, 3)
-        @test bitwise_scan(a, g, 1, 5) == (33, 4)
-        @test bitwise_scan(a, h, 1, 5) == (33, 2)
-        @test bitwise_scan(a, i, 1, 5) == (32, 1) # actually best is 32,2
-        @test bitwise_scan(a, j, 1, 5) == (31, 2) # actually best is 32,3
+        @test bitwise_scan(a, b, 1, 0) == (4, 16, NaN, NaN)
+        @test bitwise_scan(a, c, 1, 0) == (5, 16, NaN, NaN)
+        @test bitwise_scan(a, d, 1, 0) == (33, 4, NaN, NaN)
+        @test bitwise_scan(a, e, 1, 0) == (31, 4, NaN, NaN) # actually best is 32,4
+        @test bitwise_scan(a, f, 1, 5) == (32, 3, NaN, NaN)
+        @test bitwise_scan(a, g, 1, 5) == (33, 4, NaN, NaN)
+        @test bitwise_scan(a, h, 1, 5) == (33, 2, NaN, NaN)
+        @test bitwise_scan(a, i, 1, 5) == (32, 1, NaN, NaN) # actually best is 32,2
+        @test bitwise_scan(a, j, 1, 5) == (31, 2, NaN, NaN) # actually best is 32,3
     end
 end
